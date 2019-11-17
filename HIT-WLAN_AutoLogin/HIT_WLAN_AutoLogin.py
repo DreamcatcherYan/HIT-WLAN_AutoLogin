08baf4f6496ee6099ebf3b584555ba899cb1f894
GuanJiannan <guanjiannan@outlook.com>
2019-11-17 19:21:51 +0800

    fix tab

--- HIT-WLAN_AutoLogin/HIT_WLAN_AutoLogin.py
+++ HIT-WLAN_AutoLogin/HIT_WLAN_AutoLogin.py
@@ -13,10 +13,9 @@ def auto_login(userId, password):
         print('Error! You may already login!')
         exit(0)
 
-    headers = {
-	'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	'Referer':response.text.strip().lstrip("<script>top.self.location.href='").rstrip("'</script>"),
-    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) TkinterleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'
+    headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+               'Referer':response.text.strip().lstrip("<script>top.self.location.href='").rstrip("'</script>"),
+               'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) TkinterleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'
     }
 
     url = "http://202.118.253.94:8080/eportal/InterFace.do?method=login"
@@ -42,8 +41,8 @@ def read_data():
     try:
         with open(r'data.json', 'r') as f:
             data = json.load(f)
-    except FileNotFoundError:
-        return 0
+        except FileNotFoundError:
+            return 0
 
     return data
 
@@ -51,60 +50,60 @@ def read_data():
 def save_data():
     global username_entry
     global password_entry
-
+    
     username=username_entry.get()
     password=password_entry.get()
-
+    
     with open(r'data.json', 'w') as f:
         json.dump('$'.join((username, password)), f)
-
+    
     auto_login(username,password)
 
 
 
 def draw_window():
     ''' 绘制初次登陆界面 '''
-
-
+    
+    
     ''' 设置窗口 '''
     window = tkinter.Tk()
     window.title('HIT-WLAN Auto Login')
     window.geometry('400x500')
-
+    
     ''' Logo '''
     Logo=tkinter.PhotoImage(file="HIT_LOGO.png")  
     logo_label = tkinter.Label(window,image=Logo)
     logo_label.pack()
-
+    
     ''' 标题 '''
     title_label=tkinter.Label(window,
                               text='HIT-WLAN Auto Login',
                               font=('Consolas',18))
     title_label.pack()
-
+    
     ''' 文本框标签 '''
     username_label = tkinter.Label(window, text="学号",font=('等线',12))
     password_label = tkinter.Label(window, text="密码",font=('等线',12))
-
+    
     ''' 文本框 '''
     username = tkinter.StringVar()
     password = tkinter.StringVar()
-
+    
     global username_entry
     global password_entry
     username_entry = tkinter.Entry(window, show=None, textvariable=username)
     password_entry = tkinter.Entry(window, show='*', textvariable=password)
-
+    
     ''' 显示文本框及其标签 '''
     username_label.pack()
     username_entry.pack()
     password_label.pack()
     password_entry.pack()
-
+    
     ''' 按钮 '''
     button = tkinter.Button(window, text='Save', font=('Consolas',10),command=save_data)
     button.pack()
-
+    
     tkinter.mainloop()
 
 
